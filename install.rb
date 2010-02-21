5794aa1c71710bfbf295ddc30e4412c5c07af5ea
Boguś Tarnowski <bogus@3is.pl>
2010-02-21 19:34:26 +0100

    correct install / remove

--- install.rb
+++ install.rb
@@ -1,7 +1,7 @@
 
 puts "Copying files..."
 unless File.exists?(RAILS_ROOT + '/public/javascripts/tafel_tree_view/Tree-optimized.js')
-  ['/public', '/public/javascripts/tafel_tree_view', '/public/stylesheets/tafel_tree_view', '/public/images/tafel_tree_view'].each do |dir|
+  ['/public/javascripts/tafel_tree_view', '/public/stylesheets/tafel_tree_view', '/public/images/tafel_tree_view'].each do |dir|
     source = File.join(directory,dir)
     dest = RAILS_ROOT + dir
     FileUtils.mkdir_p(dest)
--- uninstall.rb
+++ uninstall.rb
@@ -1,7 +1,7 @@
 
 puts "Deleting files..."
 if File.exists?(RAILS_ROOT + '/public/javascripts/tafel_tree_view/Tree-optimized.js')
-  ['/public', '/public/javascripts/tafel_tree_view', '/public/stylesheets/tafel_tree_view', '/public/images/tafel_tree_view'].each do |dir|
+  ['/public/javascripts/tafel_tree_view', '/public/stylesheets/tafel_tree_view', '/public/images/tafel_tree_view'].each do |dir|
     dest = RAILS_ROOT + dir
     FileUtils.remove_dir(dest)
   end
